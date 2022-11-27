b7474b73906b33a95b60772c7faad8e171ddac6e
Mauro Scheltens <mauroscheltens@hotmail.com>
2022-11-27 20:44:10 +0100

    feat: homepage and routes for profile and posts pages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Models\Post;
+use App\Models\User;
 
 // Non-login routes
 Route::get('/', function() {
@@ -18,5 +20,26 @@ Route::get('logout', [AuthController::class, 'logOut'])->name('logOut');
 // Logged in users routes
 Route::get('/home', function() {
     $user = Auth::user();
-    return view('home', ['user' => $user]);
-})->middleware('auth');
+
+    $lastPost = Post::orderBy('id', 'desc')->first();
+
+    $lastPostUser = User::whereId($lastPost->user_id)->first();
+
+    $postsCount = Post::count();
+
+    return view('home', ['user' => $user, 'lastPost' => $lastPost, 'lastPostUser' => $lastPostUser, 'postsCount' => $postsCount]);
+})->middleware('auth')->name('home');
+
+Route::get('/createpost', function() {
+    return view('createPost');
+})->middleware('auth')->name('createPost');
+
+Route::get('/posts', function() {
+    return view('posts');
+})->middleware('auth')->name('postsIndex');
+
+Route::get('/profile', function() {
+    $user = Auth::user();
+
+    return view('profile', ['user' => $user]);
+})->middleware('auth')->name('profile');
