55b519c51890511b1f5d5842c10a9c47d423f9fc
Mauro Scheltens <mauroscheltens@hotmail.com>
2022-11-27 14:59:40 +0100

    feat: updated web.php with comment for routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+// Non-login routes
 Route::get('/', function() {
     return view('index');
 });
@@ -14,6 +15,7 @@ Route::get('register', [AuthController::class, 'register'])->name('register');
 Route::post('register/request', [AuthController::class, 'registerRequest'])->name('registerRequest'); 
 Route::get('logout', [AuthController::class, 'logOut'])->name('logOut');
 
+// Logged in users routes
 Route::get('/home', function() {
     $user = Auth::user();
     return view('home', ['user' => $user]);
