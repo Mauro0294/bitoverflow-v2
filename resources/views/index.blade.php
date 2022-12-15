228b45b03250a42c0e8d7e6bf93879a6d313f1b3
Mauro Scheltens <mauroscheltens@hotmail.com>
2022-12-15 11:31:35 +0100

    feat(home): added header image

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,26 +7,36 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Bitoverflow | Index</title>
 </head>
-<body class='bg-[#181818] px-24 py-12'>
-    <nav class='flex justify-between'>
-        <h2 class='text-white font-bold text-2xl'>Bit Overflow</h2>
-        <div class='flex text-white items-center text-xl font-bold'>
-            <a href="{{ route('login') }}" class='flex'><img src='icons/lock.svg' width='40px'><button>Log in</button></a>
-            <a href="{{ route('login') }}" class='ml-8'><button class='bg-white px-4 py-2 rounded-full text-black text-lg'>Get Started</button></a>
-        </div>
-    </nav>
-    <section class='mx-auto w-[850px] font-bold mt-32'>
-        <h2 class='text-7xl text-white text-center'>Betere code schrijf je,<br /> <span class='text-transparent bg-clip-text bg-gradient-to-br from-[#647DEE] to-[#7F53AC]'>samen</span></h2>
-        <p class='text-[#9BA3C7] text-center w-[535px] mx-auto mt-9'>Leer van elkaar, Op Bit-Overflow leer je van medestudenten. Je leert van studenten die precies weten hoe de opdracht in elkaar steekt.</p>
-        <div class='flex justify-center mt-12'>
-            <a href="{{ route('login') }}">
-                <button class='bg-white px-4 py-2 rounded-full text-black text-lg flex justify-center items-center'>
-                    Get Started
-                    <img src='icons/arrow2.svg' class='inline ml-2'>
-                </button>
-            </a>
-            <a href="{{ route('login') }}" class='flex ml-12'><button class='text-white'>Wie zijn wij?</button></a>
-        </div>
+<body class='bg-[#181818]'>
+    <div class='px-24 py-12'>
+        <nav class='flex justify-between'>
+            <h2 class='text-white font-bold text-2xl'>Bit Overflow</h2>
+            <div class='flex text-white items-center text-xl font-bold'>
+                <a href="{{ route('login') }}" class='flex'><img src='icons/lock.svg' width='40px'><button>Log in</button></a>
+                <a href="{{ route('login') }}" class='ml-8'><button class='bg-white px-4 py-2 rounded-full text-black text-lg'>Get Started</button></a>
+            </div>
+        </nav>
+        <section class='mx-auto w-[850px] font-bold mt-32 relative'>
+            <h2 class='text-7xl text-white text-center'>Betere code schrijf je,<br /> <span class='text-transparent bg-clip-text bg-gradient-to-br from-[#647DEE] to-[#7F53AC]'>samen</span></h2>
+            <p class='text-[#9BA3C7] text-center w-[535px] mx-auto mt-9'>Leer van elkaar, Op Bit-Overflow leer je van medestudenten. Je leert van studenten die precies weten hoe de opdracht in elkaar steekt.</p>
+            <div class='flex justify-center mt-12'>
+                <a href="{{ route('login') }}">
+                    <button class='bg-white px-4 py-2 rounded-full text-black text-lg flex justify-center items-center'>
+                        Get Started
+                        <img src='icons/arrow2.svg' class='inline ml-2'>
+                    </button>
+                </a>
+                <a href="{{ route('login') }}" class='flex ml-12'><button class='text-white'>Wie zijn wij?</button></a>
+            </div>
+            <img src='images/blue.png' style='right: -300px' class='absolute top-40'>
+        </section>
+        <section class='relative'>
+            <img src='images/gradient.png' style='bottom: -300px; left: -100px' class='absolute z-0'>
+            <img src='images/screen_mockup.svg' class='mx-auto mt-32 relative z-1'>
+        </section>
+    </div>
+    <section class='bg-[#1F1F1F] text-white mt-40 p-24'>
+        <h2 class='text-5xl'>Zeker weten dat je de juiste hulp krijgt</h2>
     </section>
 </body>
 </html>
