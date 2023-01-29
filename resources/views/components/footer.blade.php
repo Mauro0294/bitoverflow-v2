4017ff8485a536b0e12206c1942706278c87677f
Mauro Scheltens <mauroscheltens@hotmail.com>
2023-01-29 19:05:03 +0100

    feat: components for navbar and footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,11 @@
+<div class='flex items-center py-8 lg:pl-8 px-6 lg:px-0 lg:pr-24 w-full justify-between w-full'>
+    <h2 class='font-bold uppercase text-white text-lg lg:ml-48 cursor-pointer hover:text-gray-300 ease-in-out duration-300'>BitOverflow</h2>
+    <div>
+        <ul class='text-x lg:text-lg'>
+            <li class='font-bold uppercase text-white cursor-pointer hover:text-gray-300 ease-in-out duration-300'>Guidelines</li>
+            <li class='font-bold uppercase text-white cursor-pointer hover:text-gray-300 ease-in-out duration-300'>Privacy</li>
+            <li class='font-bold uppercase text-white cursor-pointer hover:text-gray-300 ease-in-out duration-300'>Disclaimer</li>
+            <li class='font-bold uppercase text-white cursor-pointer hover:text-gray-300 ease-in-out duration-300'>Bit-Academy</li>
+        </ul>
+    </div>
+</div>
